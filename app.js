3fcaf24fb3b34cfb8f51e170fcf71e696e9b2d74
Arish Arsalan <159803393+Arish-Arsalan@users.noreply.github.com>
2024-04-04 05:19:27 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict";
+//create a variable called allien_color
+let alien_color = "green";
+// . Write an if statement to test whether the alien's color is green. If its
+//   print a message that the player just earned 5 points.
+if (alien_color == "green") {
+    console.log("the player just earned 5 points.");
+}
+// write one version of this program that passees the if test and another that 
+//(the version that fails will have no output.)
+alien_color = "red";
+if (alien_color == "green") {
+    console.log(" the player just earned 5 points.");
+}
---
+++ app.ts
@@ -0,0 +1,16 @@
+//create a variable called allien_color
+let alien_color: string ="green";
+
+// . Write an if statement to test whether the alien's color is green. If its
+//   print a message that the player just earned 5 points.
+if(alien_color =="green"){
+    console.log("the player just earned 5 points.");
+}
+
+// write one version of this program that passees the if test and another that 
+//(the version that fails will have no output.)
+
+alien_color ="red";
+if(alien_color =="green"){
+    console.log(" the player just earned 5 points.");
+}
